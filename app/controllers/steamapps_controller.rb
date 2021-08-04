a99709e065c65a34aeba7c57591dc3f69849c494
kzk00001 <kzk00001@hotmail.co.jp>
2021-08-04 15:16:33 +0900

    スクレイピングのテスト

--- app/controllers/steamapps_controller.rb
+++ app/controllers/steamapps_controller.rb
@@ -11,24 +11,64 @@ class SteamappsController < ApplicationController
       charset = h.charset
       h.read
     end
-    @doc=eval(html)
-    @applist=@doc[:applist][:apps][4..8]#5個目からアプリ
+    doc=eval(html)
+    @applist=doc[:applist][:apps][4..7]#5個目からアプリ
     App.create(@applist)#rails db:reset
 
     # リストを読み出してスクレイピング
-    @applist=App.order("created_at ASC")
-    @applist.each do |app|
-      @app=app[:appid]
-      uri = "https://store.steampowered.com/app/#{@app}"
-      @doc = Nokogiri::HTML(open(uri),nil,"utf-8")
-      #アプリごと
-      @uri=[]
-      @doc.css(".highlight_screenshot_link").each do |link|
-        # link.textは、link要素（この場合はaタグ)の中の文字列を取得します。
-        @uri<<link["href"]
+    applist=App.order("created_at ASC")
+    applist.each do |app|
+      uri = "https://store.steampowered.com/app/#{app.appid}"
+      doc = Nokogiri::HTML(open(uri),nil,"utf-8")
+      #画像のURL
+      @app_info=app.appid
+      @app_info<<app.name
+
+      @image_hd=[]
+      doc.css(".highlight_screenshot_link").each do |url|
+        @image_hd<<url[:href]
+      end
+
+      @image_low=[]
+      doc.css(".highlight_strip_screenshot img").each do |url|
+        @image_low<<url.attribute("src").value
+      end
+
+      @movie=[]
+      doc.css(".highlight_movie").each do |url|
+        @movie<<url[:"data-webm-hd-source"]
+      end
+
+      @content=[]
+      doc.css(".game_header_image_full").each do |url|
+        @content<<url.attribute("src").value
+      end
+      @content<<doc.css(".game_description_snippet").text
+      @content<<doc.css("#userReviews .game_review_summary").text
+      @content<<doc.css(".date").text
+      @content<<doc.css("#developers_list").text
+
+      @tag=doc.css(".glance_tags_label").text
+      doc.css(".app_tag").each do |url|
+        @tag<<url.text
+      end
+      
+      doc.css("#game_area_purchase .game_purchase_price").each do |value|
+        @price=value[:"data-price-final"]
+      end
+      doc.css("#game_area_purchase .game_purchase_discount").each do |value|
+        @price=value[:"data-price-final"]
+      end
+      doc.css("#game_area_purchase .discount_pct").each do |value|
+        @price<<value.text
+      end
+      doc.css("#game_area_purchase .discount_original_price").each do |value|
+        @price<<value.text
+      end
+      doc.css("#game_area_purchase .discount_final_price").each do |value|
+        @price<<value.text
       end
     end
-
 
   end
 end

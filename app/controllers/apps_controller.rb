42e700f009f2907cdc160b44f0ecdb948bbf2192
kzk00001 <kzk00001@hotmail.co.jp>
2021-08-19 14:14:38 +0900

    重複するレコードの保存を回避

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -45,7 +45,6 @@ class AppsController < ApplicationController
   end
 
   def get_app
-    # Applist.destroy_all
     uri = "http://api.steampowered.com/ISteamApps/GetAppList/v2"
     charset = nil
     html = open(uri) do |h|
@@ -53,77 +52,84 @@ class AppsController < ApplicationController
       h.read
     end
     doc=eval(html)
-    applists=doc[:applist][:apps][4..8]#5個目からアプリ
+    applists=doc[:applist][:apps][4..20]#5個目からアプリ
     applists.each do |applist|
       url = "https://store.steampowered.com/app/#{applist[:appid]}"
       doc = Nokogiri::HTML(open(url),nil,"utf-8")
-      #スクレイピング
       price={}
       nokogiri_target(doc,price,:game_purchase_price,".game_purchase_price",:"data-price-final")
       nokogiri_target(doc,price,:game_purchase_price,".game_purchase_discount",:"data-price-final")
-
       unless price[:game_purchase_price].nil?
+        unless Applist.where(appid: applist[:appid]).exists?
+          applist=Applist.create(applist)
+        else
+          applist=Applist.find_by(applist)
+        end
         price[:game_purchase_price]=price[:game_purchase_price].to_i/100
         price[:game_purchase_price]="¥ "+price[:game_purchase_price].to_s
-        applist=Applist.create(applist)
         nokogiri_text(doc,price,:discount_pct,"#game_area_purchase .discount_pct")
         nokogiri_text(doc,price,:discount_original_price,"#game_area_purchase .discount_original_price")
         nokogiri_text(doc,price,:discount_final_price,"#game_area_purchase .discount_final_price")
         price[:applist_id]=applist.id
         Price.create(price)
+        
+        unless Applist.where(appid: applist[:appid]).exists?
+          content={}
+          nokogiri_text(doc,content,:glance_detail,".glance_details")
+          nokogiri_src(doc,content,:header_image_url,".game_header_image_full")
+          nokogiri_text(doc,content,:description,".game_description_snippet")
+          nokogiri_text(doc,content,:review_summary,"#userReviews .game_review_summary")
+          nokogiri_text(doc,content,:review_summary,"#userReviews .summary")
+          nokogiri_text(doc,content,:release_date,".date")
+          nokogiri_text(doc,content,:developer,"#developers_list")
+          content[:applist_id]=applist.id
+          Content.create(content)
 
-        content={}
-        nokogiri_text(doc,content,:glance_detail,".glance_details")
-        nokogiri_src(doc,content,:header_image_url,".game_header_image_full")
-        nokogiri_text(doc,content,:description,".game_description_snippet")
-        nokogiri_text(doc,content,:review_summary,"#userReviews .game_review_summary")
-        nokogiri_text(doc,content,:review_summary,"#userReviews .summary")
-        nokogiri_text(doc,content,:release_date,".date")
-        nokogiri_text(doc,content,:developer,"#developers_list")
-        content[:applist_id]=applist.id
-        Content.create(content)
+          doc.css(".highlight_screenshot_link").each do |value|
+            screenshot_hd={}
+            screenshot_hd[:url]=value[:href]
+            screenshot_hd[:applist_id]=applist.id
+            ScreenshotHd.create(screenshot_hd)
+          end
 
-        doc.css(".highlight_screenshot_link").each do |value|
-          screenshot_hd={}
-          screenshot_hd[:url]=value[:href]
-          screenshot_hd[:applist_id]=applist.id
-          ScreenshotHd.create(screenshot_hd)
-        end
+          doc.css(".highlight_strip_screenshot img").each do |value|
+            screenshot_poor={}
+            screenshot_poor[:url]=value.attribute("src").value
+            screenshot_poor[:applist_id]=applist.id
+            ScreenshotPoor.create(screenshot_poor)
+          end
 
-        doc.css(".highlight_strip_screenshot img").each do |value|
-          screenshot_poor={}
-          screenshot_poor[:url]=value.attribute("src").value
-          screenshot_poor[:applist_id]=applist.id
-          ScreenshotPoor.create(screenshot_poor)
-        end
+          doc.css(".highlight_movie").each do |value|
+            movie={}
+            movie[:url]=value[:"data-webm-hd-source"]
+            movie[:applist_id]=applist.id
+            Movie.create(movie)
+          end
 
-        doc.css(".highlight_movie").each do |value|
-          movie={}
-          movie[:url]=value[:"data-webm-hd-source"]
-          movie[:applist_id]=applist.id
-          Movie.create(movie)
-        end
-
-        doc.css(".app_tag").each do |value|
-          unless value.text.strip.include?("+")
-            tag={}
-            tag[:name]=value.text.strip
-            if Tag.where(tag).exists?
-              tag=Tag.find_by(tag)
-            else
-              tag=Tag.create(tag)
+          doc.css(".app_tag").each do |value|
+            unless value.text.strip.include?("+")
+              tag={}
+              tag[:name]=value.text.strip
+              if Tag.where(tag).exists?
+                tag=Tag.find_by(tag)
+              else
+                tag=Tag.create(tag)
+              end
+              ApplistTag.create(applist_id:applist.id,tag_id:tag.id)
             end
-            ApplistTag.create(applist_id:applist.id,tag_id:tag.id)
           end
         end
 
       else
-        nokogiri_text(doc,applist,:release_date,".date")
-        nokogiri_text(doc,applist,:price,".game_purchase_price")
-        nokogiri_text(doc,applist,:review_summary,"#userReviews .game_review_summary")
-        nokogiri_text(doc,applist,:review_summary,"#userReviews .summary")
-        DiscardedApplist.create(applist)
+        unless DiscardedApplist.where(appid: applist[:appid]).exists?
+          nokogiri_text(doc,applist,:release_date,".date")
+          nokogiri_text(doc,applist,:price,".game_purchase_price")
+          nokogiri_text(doc,applist,:review_summary,"#userReviews .game_review_summary")
+          nokogiri_text(doc,applist,:review_summary,"#userReviews .summary")
+          DiscardedApplist.create(applist)
+        end
       end
+
     end
   end
 
